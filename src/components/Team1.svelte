eadff676f840983e9815ee5c93652a2c54711485
IteraMedia <iteramedia@IteraMedias-MacBook-Pro.local>
2024-01-09 12:10:51 +0100

    test

--- src/components/Team1.svelte
+++ src/components/Team1.svelte
@@ -2,8 +2,15 @@
 
 </script>
 
-<section>
-    <h1>Team 1</h1>
+<section class="bg-blue-500">
+    <h1 class="text-blue-100">Team 11</h1>
     <p>Team 1 is the best team!</p>
     <p>Live share test</p>
+    <p>trond tester</p>
 </section>
+
+<style>
+    h1 {
+        color: Yellow;
+    }
+</style>
